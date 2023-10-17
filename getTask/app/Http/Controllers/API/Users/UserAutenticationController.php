d1a549471c59903fbbfe86fa225dc7a558575e61
João Paulo <78809245+JoaoPaulo29@users.noreply.github.com>
2023-10-17 17:00:04 +0100

    login

--- getTask/app/Http/Controllers/API/Users/UserAutenticationController.php
+++ getTask/app/Http/Controllers/API/Users/UserAutenticationController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\API\Users;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller; 
 use Illuminate\Http\Request;
 
 class UserAutenticationController extends Controller
 {
-    //
+    public function login(Request $request){
+        if (!auth()->attempt(['email'=>$request->input('email'),'password'=>$request->input('password')])) {
+            return response(['message' => 'try again'],401);
+        }
+        $token = auth()->user()->createToken('auth-token')->plainTextToken;
+    }
 }
